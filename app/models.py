89c6b484068cc0057a3139d652f2505ef23d4c05
Paras <parassinghal1@gmail.com>
2024-02-25 19:14:49 +0530

    Base project added

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class Meaning(BaseModel):
+    part_of_speech: str
+    definitions: list[str]
+
+class DictionaryMeaning(BaseModel):
+    meanings: list[Meaning]
+    source: list[str]
